86cf29ec0be1e43bf00bf7854ee4d4fbfa57322f
Andreas Krohn <hamburger1984@gmail.com>
2017-09-25 10:38:46 +0200

    MathPojo was not passing context parameter to enrichText.

--- app/src/main/java/fr/neamar/kiss/result/MathResult.java
+++ app/src/main/java/fr/neamar/kiss/result/MathResult.java
@@ -1,8 +1,6 @@
 package fr.neamar.kiss.result;
 
-import android.app.SearchManager;
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.view.View;
 import android.widget.ImageView;
@@ -11,14 +9,13 @@ import android.widget.TextView;
 import fr.neamar.kiss.R;
 import fr.neamar.kiss.pojo.MathPojo;
 
-public class MathResult extends Result{
+public class MathResult extends Result {
 
     private final MathPojo mathPojo;
 
     public MathResult(MathPojo mathPojo) {
         super();
         this.pojo = this.mathPojo = mathPojo;
-
     }
 
     @Override
@@ -27,7 +24,7 @@ public class MathResult extends Result{
             v = inflateFromId(context, R.layout.item_math);
         TextView appName = (TextView) v.findViewById(R.id.item_math_text);
         String text = context.getString(R.string.ui_item_search);
-        appName.setText(enrichText(String.format(text, this.pojo.name, "{" + mathPojo.result + "}")));
+        appName.setText(enrichText(String.format(text, this.pojo.name, "{" + mathPojo.result + "}"), context));
 
         ((ImageView) v.findViewById(R.id.item_math_icon)).setColorFilter(getThemeFillColor(context), PorterDuff.Mode.SRC_IN);
         return v;
